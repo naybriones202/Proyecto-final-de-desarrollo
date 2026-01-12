7054e4088ddf047dd1d59d8373faeade4b932aad
naybriones202 <nbriones4760@utm.edu.ec>
2026-01-12 10:29:32 -0500

    Primer commint

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,4 +1,4 @@
-const API = 'http://localhost:3000';
+const API = 'https://proyecto-final-de-desarrollo.onrender.com';
 
 // Elementos Globales
 const loginSection = document.getElementById('login-section');
